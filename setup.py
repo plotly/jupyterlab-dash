85fd6f87679272dbfaceefdcc64390d29ae004ae
Grant Nestor <grantnestor@gmail.com>
2019-03-05 15:57:25 -0800

    Add dash as a requirement

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='jupyterlab_dash',
     version='0.1',
-    packages=['jupyterlab_dash']
+    packages=['jupyterlab_dash'],
+    install_requires=['dash']
 )
