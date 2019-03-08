6733ebae960c920dd50cd7d50e4717ec0003ce58
Jon Mease <jon.mease@gmail.com>
2019-03-08 18:33:29 -0500

    Commit lib/ to make it easier to install from the git repo

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+import { JupyterLabPlugin } from '@jupyterlab/application';
+import '../style/index.css';
+/**
+ * Initialization data for the jupyterlab_dash extension.
+ */
+declare const extension: JupyterLabPlugin<void>;
+export default extension;
